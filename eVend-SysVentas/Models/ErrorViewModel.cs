4f2b8cf61c97dff1b8904406547b941a4f881b05
JaremGallegos <139504059+JaremGallegos@users.noreply.github.com>
2024-11-22 09:24:12 -0500

    init: inicialización del proyecto
    Este commit inicializa el repositorio con:
    - La estructura básica de carpetas.
    - Un archivo README con información inicial.
    - Configuración inicial para herramientas como Gitignore, ESLint, o Prettier.

---
+++ eVend-SysVentas/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace eVend_SysVentas.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
